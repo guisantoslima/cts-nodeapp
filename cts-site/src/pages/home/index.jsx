5321d3db5b36986722e43b0fad9ef3eeccbf3f5a
gsantoli <guilherme.lima@live.com>
2024-11-09 23:09:28 -0300

    v2

--- cts-site/src/pages/home/index.jsx
+++ cts-site/src/pages/home/index.jsx
@@ -1,16 +1,50 @@
 import './style.css'
+import Trash from '../../assets/trash.png'
 
 function Home() {
 
+
+  const users = [{
+    id: '2024110901',
+    name: 'Guilherme',
+    email: 'email01@mail.com',
+    password: '12345'
+  }, {
+    id: '2024110902',
+    name: 'Ana Carolina',
+    email: 'email02@mail.com',
+    password: '12345'
+  },
+  {
+    id: '2024110903',
+    name: 'Josefa',
+    email: 'email03@mail.com',
+    password: '12345'
+  }
+  ]
   return (
-    <>
-      <div>
-        <h1>HeloWolrd</h1>
-      </div>
-      <p className="read-the-docs">
-        Sample Project
-      </p>
-    </>
+
+    <div className='container'>
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input placeholder='Nome' name='name' type='text' />
+        <input placeholder='Email' name='email' type='email' />
+        <input placeholder='Senha' name='password' type='password' />
+        <button type='button'>Cadastrar</button>
+      </form>
+      {users.map(user => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome:      <span>{user.name}</span></p>
+            <p>Email:     <span>{user.email}</span></p>
+            <p>Password:  <span>{user.password}</span></p>
+          </div>
+          <button>
+            <img src='{Trash}' />
+          </button>
+        </div>
+      ))}
+    </div>
   )
 }
 
