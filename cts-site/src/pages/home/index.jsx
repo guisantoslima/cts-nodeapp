53052d6b8ecb4caa999765a7da08a1ae7ced39cb
gsantoli <guilherme.lima@live.com>
2024-11-09 21:32:46 -0300

    principal

---
+++ cts-site/src/pages/home/index.jsx
@@ -0,0 +1,17 @@
+import './style.css'
+
+function Home() {
+
+  return (
+    <>
+      <div>
+        <h1>HeloWolrd</h1>
+      </div>
+      <p className="read-the-docs">
+        Sample Project
+      </p>
+    </>
+  )
+}
+
+export default Home
